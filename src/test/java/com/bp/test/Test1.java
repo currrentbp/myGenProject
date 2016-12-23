c6be01f594480827bc6587f1e6bdcc1c3760b40a
wb-baopan <wb-baopan@le.com>
2016-12-23 09:55:38 +0800

    sssss

---
+++ src/test/java/com/bp/test/Test1.java
@@ -0,0 +1,15 @@
+package com.bp.test;
+
+import org.junit.Test;
+
+/**
+ * Created by issuser on 2016/12/23.
+ */
+public class Test1 {
+
+
+    @Test
+    public void test11(){
+        System.out.println("======");
+    }
+}
