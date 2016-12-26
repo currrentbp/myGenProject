3cfe9247e890e264c8e0806aeee11b23addba2a2
wb-baopan <wb-baopan@le.com>
2016-12-26 10:45:44 +0800

    修改了关于写入文件的一个方法，以前理解错误（追加错误，写成了自动刷新了。。），密码本：纯数字的写好了一个

--- src/main/java/com/bp/util/all/PWDGenUtil.java
+++ src/main/java/com/bp/util/all/PWDGenUtil.java
@@ -2,7 +2,9 @@ package com.bp.util.all;
 
 import com.bp.staticValue.StaticValue;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -14,8 +16,16 @@ import java.util.List;
 public class PWDGenUtil {
 
     public String nums = "0123456789";
-    public List<String> pwds = new ArrayList<String>();
+    private List<String> pwds = new ArrayList<String>();
+    private LinkedList<String> linkedList = new LinkedList<String>();
 
+
+    /**
+     * 产生密码本：纯粹数字的，
+     * 长度超过6位就速度非常的慢，而且经常卡机
+     * @param len 长度
+     * @param path 生成的路径
+     */
     public void genPuleNumDic(int len, String path) {
         int index = StaticValue.START_INDEX.getValue();
         boolean flag = false;
@@ -50,11 +60,26 @@ public class PWDGenUtil {
         }
 
         System.out.println(pwds);
+        StringBuilder sb = new StringBuilder();
+
+        for (String pwd:pwds) {
+            sb.append(pwd+"\n");
+        }
+        try {
+            StreamUtil.writeSomethingToFile(sb.toString(),path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void test1(int len){
+
     }
 
     public static void main(String[] args) {
 
         PWDGenUtil pu = new PWDGenUtil();
-        pu.genPuleNumDic(6, "");
+        pu.genPuleNumDic(5, "E:\\tmp\\bp.dic");
     }
 }
