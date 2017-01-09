51027702f6ffea55d7ffc16a5089e809cb7da458
wb-baopan <wb-baopan@le.com>
2017-01-09 10:59:26 +0800

    获取线程的返回值2

--- src/main/java/com/bp/thread/UseExecutorGetResult.java
+++ src/main/java/com/bp/thread/UseExecutorGetResult.java
@@ -12,19 +12,36 @@ import java.util.concurrent.*;
 public class UseExecutorGetResult {
 
     private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private  List<String> list = new ArrayList<String>();
+    private static int count = 1;
 
     public static void main(String [] args){
         UseExecutorGetResult useExecutorGetResult = new UseExecutorGetResult();
+        //这个例子没有考虑线程安全问题，只是单纯的通过线程拿到数据的demo
         useExecutorGetResult.getResult();
+        useExecutorGetResult.getResult();
+        useExecutorGetResult.getResult();
+        useExecutorGetResult.getResult();
+        //必须手动关闭线程池
+        useExecutorGetResult.executorService.shutdown();
+
+        /*
+        list1:[baopan1]
+list1:[baopan1, baopan2]
+list1:[baopan1, baopan2, baopan3]
+list1:[baopan1, baopan2, baopan3, baopan4]
+//主线程一直没有关闭
+         */
     }
 
     public void getResult(){
-        final List<String> list = new ArrayList<>();
+
 
         Callable<List<String>> task = new Callable<List<String>>() {
             @Override
             public List<String> call() throws Exception {
-                list.add("baopan1");
+                list.add("baopan"+count);
+                count++;
                 return list;
             }
         };
