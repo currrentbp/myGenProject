35f9234f357d13cff46014c054329b6f01bd4547
wb-baopan <wb-baopan@le.com>
2017-01-09 19:25:47 +0800

    获取线程的返回值3

--- src/main/java/com/bp/thread/UseExecutorGetResult.java
+++ src/main/java/com/bp/thread/UseExecutorGetResult.java
@@ -6,16 +6,17 @@ import java.util.concurrent.*;
 
 /**
  * 通过callable获取线程返回的结果
+ *
  * @author current_bp
  * @createTime 20170109
  */
 public class UseExecutorGetResult {
 
     private final ExecutorService executorService = Executors.newFixedThreadPool(10);
-    private  List<String> list = new ArrayList<String>();
+    private List<String> list = new ArrayList<String>();
     private static int count = 1;
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         UseExecutorGetResult useExecutorGetResult = new UseExecutorGetResult();
         //这个例子没有考虑线程安全问题，只是单纯的通过线程拿到数据的demo
         useExecutorGetResult.getResult();
@@ -34,13 +35,13 @@ list1:[baopan1, baopan2, baopan3, baopan4]
          */
     }
 
-    public void getResult(){
+    public void getResult() {
 
 
         Callable<List<String>> task = new Callable<List<String>>() {
             @Override
             public List<String> call() throws Exception {
-                list.add("baopan"+count);
+                list.add("baopan" + count);
                 count++;
                 return list;
             }
@@ -50,7 +51,7 @@ list1:[baopan1, baopan2, baopan3, baopan4]
 
         try {
             List<String> list1 = future.get();
-            System.out.println("list1:"+list1);
+            System.out.println("list1:" + list1 + " hashCode:" + list1.hashCode());
 
         } catch (InterruptedException e) {
             e.printStackTrace();
