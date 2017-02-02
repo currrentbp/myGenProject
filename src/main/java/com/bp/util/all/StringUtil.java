5f76ce0d81e298b24cd6bc69469bf69eb2d1ef86
current_bp <1403271248@qq.com>
2017-02-02 17:05:31 +0800

    22

--- src/main/java/com/bp/util/all/StringUtil.java
+++ src/main/java/com/bp/util/all/StringUtil.java
@@ -4,353 +4,326 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 
  * 关于字符串的类
- * 
+ *
  * @author current_bp
  * @createTime 20160513
- *
  */
 
 public class StringUtil {
 
-	/**
-	 * 删除字符串的最后一个字符
-	 * 
-	 * @param s
-	 * @return
-	 */
-	public static String deleteLast(String s) {
+    /**
+     * 删除字符串的最后一个字符
+     *
+     * @param s 原数据
+     * @return 最终数据
+     */
+    public static String deleteLast(String s) {
+        if (null == s) {
+            return null;
+        }
 
-		if (null == s) {
-			return "";
-		} else if (s.length() >= 1) {
-			StringBuffer sb = new StringBuffer(s);
+        if ("".equals(s)) {
+            return "";
+        }
 
-			return sb.deleteCharAt(sb.length() - 1).toString();
-		} else {
-			return "";
-		}
-	}
+        StringBuffer sb = new StringBuffer(s);
 
-	/**
-	 * 对一个字符串删除指定的字符或者字符串,替换的内容默认为空串,
-	 * 
-	 * @param resource
-	 *            原字符串
-	 * @param replaceed
-	 *            被替换的部分，数组形式，如：new String[]{"\\(","\\)"}
-	 * @return
-	 */
-	public static String getStringWithOutSome(String resource, String[] replaceed) {
-		String temp = resource;
-		for (int i = 0; i < replaceed.length; i++) {
-			temp = getStringWithOutSome(temp, replaceed[i]);
-		}
+        return sb.deleteCharAt(sb.length() - 1).toString();
+    }
 
-		return temp;
-	}
+    /**
+     * 对一个字符串删除指定的字符或者字符串,替换的内容默认为空串,
+     *
+     * @param resource  原字符串
+     * @param replaceed 被替换的部分，数组形式，如：new String[]{"\\(","\\)"}
+     * @return
+     */
+    public static String getStringWithOutSome(String resource, String[] replaceed) {
+        String temp = resource;
+        for (int i = 0; i < replaceed.length; i++) {
+            temp = getStringWithOutSome(temp, replaceed[i]);
+        }
 
-	/**
-	 * 
-	 * 一个字符串删除指定的字符或者字符串,替换的内容默认为空串, 注意特殊字符，如（"\\("，）
-	 * 
-	 * @param resource
-	 *            原字符串
-	 * @param replaceed
-	 *            被替换的部分
-	 * @return
-	 */
-	public static String getStringWithOutSome(String resource, String replaceed) {
-		return getStringWithOutSome(resource, "", replaceed);
-	}
+        return temp;
+    }
 
-	/**
-	 * 一个字符串删除指定的字符或者字符串
-	 * 
-	 * @param resource
-	 *            原字符串
-	 * @param replace
-	 *            要替换成的部分
-	 * @param replaceed
-	 *            被替换的部分
-	 * @return
-	 */
-	public static String getStringWithOutSome(String resource, String replace, String replaceed) {
-		String result = "";
+    /**
+     * 一个字符串删除指定的字符或者字符串,替换的内容默认为空串, 注意特殊字符，如（"\\("，）
+     *
+     * @param resource  原字符串
+     * @param replaceed 被替换的部分
+     * @return
+     */
+    public static String getStringWithOutSome(String resource, String replaceed) {
+        return getStringWithOutSome(resource, "", replaceed);
+    }
 
-		// 如果为空或者空串，就不需要替换了
-		if (null == resource || "".equals(resource)) {
-			return resource;
-		}
+    /**
+     * 一个字符串删除指定的字符或者字符串
+     *
+     * @param resource  原字符串
+     * @param replace   要替换成的部分
+     * @param replaceed 被替换的部分
+     * @return 结果
+     */
+    public static String getStringWithOutSome(String resource, String replace, String replaceed) {
+        String result = "";
 
-		if (null == replaceed || "".equals(replaceed)) {
-			throw new RuntimeException("被替换的部分不能为空和空串！");
-		}
+        // 如果为空或者空串，就不需要替换了
+        if (null == resource || "".equals(resource)) {
+            return resource;
+        }
 
-		if (null == replace) {
-			throw new RuntimeException("替换内容不能为null！");
-		}
+        if (null == replaceed || "".equals(replaceed)) {
+            throw new RuntimeException("被替换的部分不能为空和空串！");
+        }
 
-		result = resource;
+        if (null == replace) {
+            throw new RuntimeException("替换内容不能为null！");
+        }
 
-		return result.replaceAll(replaceed, replace);
-	}
+        result = resource;
 
-	/**
-	 * 
-	 * 字符串左边填充0
-	 * 
-	 * @param length
-	 *            结果字符串的长度
-	 * @param fill
-	 *            填充的内容
-	 */
-	public static String fillBySome(int length, int value) {
-		return fillBySome(length, "" + value, "0");
-	}
+        return result.replaceAll(replaceed, replace);
+    }
 
-	/**
-	 * 
-	 * 字符串左边填充0
-	 * 
-	 * @param length
-	 *            结果字符串的长度
-	 * @param fill
-	 *            填充的内容
-	 */
-	public static String fillBySome(int length, String value) {
-		return fillBySome(length, value, "0");
-	}
+    /**
+     * 字符串左边填充0
+     *
+     * @param length 结果字符串的长度
+     */
+    public static String fillBySome(int length, int value) {
+        return fillBySome(length, "" + value, "0");
+    }
 
-	/**
-	 * 左边填充字符串
-	 * 
-	 * @param length
-	 *            结果字符串的长度
-	 * @param value
-	 *            给定的字符串
-	 * @param fill
-	 *            填充的内容
-	 * @return
-	 */
-	public static String fillBySome(int length, String value, String fill) {
-		return fillBySome(length, value, fill, 0);
-	}
+    /**
+     * 字符串左边填充0
+     *
+     * @param length 结果字符串的长度
+     */
+    public static String fillBySome(int length, String value) {
+        return fillBySome(length, value, "0");
+    }
 
-	/**
-	 * 填充字符串
-	 * 
-	 * @param length
-	 *            结果字符串的长度
-	 * @param value
-	 *            给定的字符串
-	 * @param fill
-	 *            填充的内容
-	 * @param option
-	 *            位置：0：左边，1：右边
-	 * @return
-	 */
-	public static String fillBySome(int length, String value, String fill, int option) {
-		if (null == value) {
-			value = "";
-		}
-		if (null == fill) {
-			throw new RuntimeException("填充内容不能为null!");
-		}
+    /**
+     * 左边填充字符串
+     *
+     * @param length 结果字符串的长度
+     * @param value  给定的字符串
+     * @param fill   填充的内容
+     * @return
+     */
+    public static String fillBySome(int length, String value, String fill) {
+        return fillBySome(length, value, fill, 0);
+    }
 
-		String result = "" + value;
-		StringBuffer temp = new StringBuffer("");
-		StringBuffer t1 = new StringBuffer("");
+    /**
+     * 填充字符串
+     *
+     * @param length 结果字符串的长度
+     * @param value  给定的字符串
+     * @param fill   填充的内容
+     * @param option 位置：0：左边，1：右边
+     * @return
+     */
+    public static String fillBySome(int length, String value, String fill, int option) {
+        if (null == value) {
+            value = "";
+        }
+        if (null == fill) {
+            throw new RuntimeException("填充内容不能为null!");
+        }
 
-		// 如果大量的填充时
-		if (length >= 10) {
-			int x = length - value.length();// 剩余填充的位数
-			for (int i = 0; i < 10; i++) {
-				t1.append(fill);
-			}
-			for (int j = 0; j < x / 10; j++) {
-				temp.append(t1.toString());
-			}
-			for (int k = 0; k < x % 10; k++) {
-				temp.append(fill);
-			}
+        String result = "" + value;
+        StringBuffer temp = new StringBuffer("");
+        StringBuffer t1 = new StringBuffer("");
 
-		} else {// 填充东西比较小时
-			if (result.length() >= length) {
-			} else {
-				for (int i = 0; i < length - value.length(); i++) {
-					temp.append(fill);
-				}
-			}
-		}
+        // 如果大量的填充时
+        if (length >= 10) {
+            int x = length - value.length();// 剩余填充的位数
+            for (int i = 0; i < 10; i++) {
+                t1.append(fill);
+            }
+            for (int j = 0; j < x / 10; j++) {
+                temp.append(t1.toString());
+            }
+            for (int k = 0; k < x % 10; k++) {
+                temp.append(fill);
+            }
 
-		return option > 0 ? result + temp.toString() : temp.toString() + result;
-	}
+        } else {// 填充东西比较小时
+            if (result.length() >= length) {
+            } else {
+                for (int i = 0; i < length - value.length(); i++) {
+                    temp.append(fill);
+                }
+            }
+        }
 
-	/**
-	 * 获取首字母为大写的字符串
-	 * 
-	 * @param name
-	 * @return
-	 */
-	public static String getCaptureName(String name) {
-		if (null == name || "".equals(name)) {
-			throw new RuntimeException("该字符串为null或空串");
-		}
+        return option > 0 ? result + temp.toString() : temp.toString() + result;
+    }
 
-		return name.substring(0, 1).toUpperCase() + name.substring(1);
-	}
+    /**
+     * 获取首字母为大写的字符串
+     *
+     * @param name
+     * @return
+     */
+    public static String getCaptureName(String name) {
+        if (null == name || "".equals(name)) {
+            throw new RuntimeException("该字符串为null或空串");
+        }
 
-	/**
-	 * 根据格式分割字符串
-	 * 
-	 * @param resourceString
-	 * @param split
-	 * @return
-	 */
-	public static String[] getSplitString(String resourceString, String split) {
-		return resourceString.split(split);
-	}
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
+    }
 
-	/**
-	 * 在指定的位置插入字符串
-	 * 
-	 * @param resource
-	 *            原字符串
-	 * @param something
-	 *            需要插入的内容
-	 * @param where
-	 *            插入的位置,0:第一个位置
-	 * @return
-	 */
-	public static String insertSomethingToWhere(String resource, String something, int where) {
-		if (null == resource) {
-			throw new RuntimeException("原字符串不能为空！");
-		}
-		if (null == something) {
-			throw new RuntimeException("插入内容不能为空！");
-		}
-		if (resource.length() < where || where < 0) {
-			throw new IndexOutOfBoundsException("插入的位置越界！");
-		}
+    /**
+     * 根据格式分割字符串
+     *
+     * @param resourceString
+     * @param split
+     * @return
+     */
+    public static String[] getSplitString(String resourceString, String split) {
+        return resourceString.split(split);
+    }
 
-		StringBuffer sb = new StringBuffer(resource);
-		sb.insert(where, something);
+    /**
+     * 在指定的位置插入字符串
+     *
+     * @param resource  原字符串
+     * @param something 需要插入的内容
+     * @param where     插入的位置,0:第一个位置
+     * @return
+     */
+    public static String insertSomethingToWhere(String resource, String something, int where) {
+        if (null == resource) {
+            throw new RuntimeException("原字符串不能为空！");
+        }
+        if (null == something) {
+            throw new RuntimeException("插入内容不能为空！");
+        }
+        if (resource.length() < where || where < 0) {
+            throw new IndexOutOfBoundsException("插入的位置越界！");
+        }
 
-		return sb.toString();
-	}
+        StringBuffer sb = new StringBuffer(resource);
+        sb.insert(where, something);
 
-	/**
-	 * 将字符串转为列表，根据逗号分割，删除空和null
-	 * 
-	 * @param source
-	 *            字符串
-	 * @return
-	 * @throws Exception
-	 */
-	public static List<String> stringToList(String source) throws Exception {
-		return stringToList(source, true, ",");
-	}
+        return sb.toString();
+    }
 
-	/**
-	 * 将字符串根据规则转换为列表
-	 * 
-	 * @param source
-	 *            字符串
-	 * @param deleteNull
-	 *            是否删除空和null
-	 * @param splitBy
-	 *            根据什么字符分割
-	 * @return
-	 * @throws Exception
-	 */
-	public static List<String> stringToList(String source, boolean deleteNull, String splitBy) throws Exception {
-		CheckUtil.isEmpty("source", source);
-		if (null == splitBy || "".equals(splitBy)) {
-			throw new RuntimeException("splitBy is empty!");
-		}
+    /**
+     * 将字符串转为列表，根据逗号分割，删除空和null
+     *
+     * @param source 字符串
+     * @return
+     * @throws Exception
+     */
+    public static List<String> stringToList(String source) throws Exception {
+        return stringToList(source, true, ",");
+    }
 
-		String[] s = source.split(splitBy);
-		List<String> result = new ArrayList<String>();
+    /**
+     * 将字符串根据规则转换为列表
+     *
+     * @param source     字符串
+     * @param deleteNull 是否删除空和null
+     * @param splitBy    根据什么字符分割
+     * @return
+     * @throws Exception
+     */
+    public static List<String> stringToList(String source, boolean deleteNull, String splitBy) throws Exception {
+        CheckUtil.isEmpty("source", source);
+        if (null == splitBy || "".equals(splitBy)) {
+            throw new RuntimeException("splitBy is empty!");
+        }
 
-		for (String s1 : s) {
-			if (deleteNull) {
-				if (!CheckUtil.isEmpty(s1)) {
-					result.add(s1);
-				}
-			} else {
-				result.add(s1);
-			}
-		}
-		return result;
-	}
-	
-	
-	/**
-	 * 将字母转换成数字
-	 * @param letter
-	 * @return
-	 */
-	public static Integer letter2Int(String letter){
-		if(null == letter){
-			return -1;
-		}
-		return letter2Int(letter.charAt(0));
-	}
-	/**
-	 * 将字母转换成数字
-	 * @param letter
-	 * @return
-	 */
-	public static Integer letter2Int(char letter){
-		return (int)letter;
-	}
-	
-	/**
-	 * 将数字转为字母
-	 * @param num
-	 * @return
-	 */
-	public static char int2Letter(int num){
-		return (char)num;
-	}
+        String[] s = source.split(splitBy);
+        List<String> result = new ArrayList<String>();
 
-	public static void main(String[] args) throws Exception {
-		
-		System.out.println(StringUtil.letter2Int("A"));
-		System.out.println(StringUtil.int2Letter(65+11));
-		
+        for (String s1 : s) {
+            if (deleteNull) {
+                if (!CheckUtil.isEmpty(s1)) {
+                    result.add(s1);
+                }
+            } else {
+                result.add(s1);
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 将字母转换成数字
+     *
+     * @param letter
+     * @return
+     */
+    public static Integer letter2Int(String letter) {
+        if (null == letter) {
+            return -1;
+        }
+        return letter2Int(letter.charAt(0));
+    }
+
+    /**
+     * 将字母转换成数字
+     *
+     * @param letter
+     * @return
+     */
+    public static Integer letter2Int(char letter) {
+        return (int) letter;
+    }
+
+    /**
+     * 将数字转为字母
+     *
+     * @param num
+     * @return
+     */
+    public static char int2Letter(int num) {
+        return (char) num;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(StringUtil.letter2Int("A"));
+        System.out.println(StringUtil.int2Letter(65 + 11));
+
 //		//将字符串转换为列表
 //		System.out.println(StringUtil.stringToList("2,2,2,2,"));
 //		System.out.println(StringUtil.stringToList("2,2,2,2,: ss",false,":"));
 
-		// System.out.println(StringUtil.insertSomethingToWhere("222", "1",
-		// -1));
-		// System.out.println(StringUtil.insertSomethingToWhere("222", "1", 0));
-		// System.out.println(StringUtil.insertSomethingToWhere("222", "1", 1));
-		// System.out.println(StringUtil.insertSomethingToWhere("222", "1", 2));
-		// System.out.println(StringUtil.insertSomethingToWhere("222", "1", 3));
-		// System.out.println(StringUtil.insertSomethingToWhere("222", "1", 4));
-		// System.out.println(StringUtil.insertSomethingToWhere("222", "11111",
-		// 2));
+        // System.out.println(StringUtil.insertSomethingToWhere("222", "1",
+        // -1));
+        // System.out.println(StringUtil.insertSomethingToWhere("222", "1", 0));
+        // System.out.println(StringUtil.insertSomethingToWhere("222", "1", 1));
+        // System.out.println(StringUtil.insertSomethingToWhere("222", "1", 2));
+        // System.out.println(StringUtil.insertSomethingToWhere("222", "1", 3));
+        // System.out.println(StringUtil.insertSomethingToWhere("222", "1", 4));
+        // System.out.println(StringUtil.insertSomethingToWhere("222", "11111",
+        // 2));
 
-		// String ss = StringUtil.getSplitString("int(11)",
-		// "int")[1].toString();
-		// System.out.println(StringUtil.getStringWithOutSome(ss, new
-		// String[]{"\\(","\\)"}));
+        // String ss = StringUtil.getSplitString("int(11)",
+        // "int")[1].toString();
+        // System.out.println(StringUtil.getStringWithOutSome(ss, new
+        // String[]{"\\(","\\)"}));
 
-		// System.out.println(StringUtil.getCaptureName("a111111a"));
-		// System.out.println(StringUtil.getCaptureName(""));
+        // System.out.println(StringUtil.getCaptureName("a111111a"));
+        // System.out.println(StringUtil.getCaptureName(""));
 
-		// System.out.println(StringUtil.fillBySome(20, "1"));
+        // System.out.println(StringUtil.fillBySome(20, "1"));
 
-		// System.out.println(StringUtil.getStringWithOutSome("123456--123123",
-		// "++", "-"));
-		// System.out.println(StringUtil.getStringWithOutSome("123456--123123",
-		// "-"));
-		// System.out.println("+++++"+StringUtil.deleteLast(null)+"====");
-		// new HashMap ();
-	}
+        // System.out.println(StringUtil.getStringWithOutSome("123456--123123",
+        // "++", "-"));
+        // System.out.println(StringUtil.getStringWithOutSome("123456--123123",
+        // "-"));
+        // System.out.println("+++++"+StringUtil.deleteLast(null)+"====");
+        // new HashMap ();
+    }
 
 }
--- src/main/java/com/bp/util/all/downLoadAndUpload/HttpDownloader1.java
+++ src/main/java/com/bp/util/all/downLoadAndUpload/HttpDownloader1.java
@@ -15,54 +15,76 @@ public class HttpDownloader1 {
     private int threadNum = 0;
     private int threadMaxNum = 5;
     private int cacheMax = 8 * 1024 * 50;//每个文件的大小50Kb
-    private static int[] progress;//0:未开始，1：下载完成，2：下载中
+    private static int[] progress;//0:未开始，1：下载完成，2：下载中，3：写入完成，4：写入中
+    private boolean isWrite = false;//是否在写入一个文件
+    private String fileName = "";//文件名称
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         HttpDownloader1 httpDownloader1 = new HttpDownloader1();
 //        httpDownloader1.initProgress(10);
+        System.out.println("----:"+httpDownloader1.writeOrReleaseFile(false));
     }
-
 
 
     /**
      * 初始化分片
+     *
      * @param fileAllSize 文件总大小
      * @return 分片
      */
     private int[] initProgress(long fileAllSize) {
         long len = fileAllSize / cacheMax;
-        if(0 != fileAllSize% cacheMax){
+        if (0 != fileAllSize % cacheMax) {
             len++;
         }
 
-        System.out.println("===>initProgress: len:"+len);
+        System.out.println("===>initProgress: len:" + len);
 
-        return new int[(int)len];
+        return new int[(int) len];
     }
 
     /**
      * 修改分片的状态
-     * @param index 分片位置
+     *
+     * @param index     分片位置
      * @param newStatus 新状态
      * @param oldStatus 旧状态
      * @return 是否成功
      */
-    private synchronized boolean changeProgress(int index ,int newStatus,int oldStatus){
+    private synchronized boolean changeProgress(int index, int newStatus, int oldStatus) {
         int oldStatus1 = progress[index];
+        //0:未开始，1：下载完成，2：下载中，3：写入完成
+        if (oldStatus == oldStatus1) {
+            return false;
+        }
 
+        progress[index] = newStatus;
 
+        return true;
+    }
 
-        return false;
+    /**
+     * 写入一个文件或者释放一个文件的写入锁
+     *
+     * @return 修改成功与否
+     */
+    public synchronized boolean writeOrReleaseFile(boolean writeOrRelease) {
+        if (writeOrRelease ^ isWrite) {
+            return false;
+        }
+
+        this.isWrite = writeOrRelease;
+
+        return true;
     }
 
 
-
-    class DownLoadThread implements Runnable{
+    class DownLoadThread implements Runnable {
         private int start = 0;
         private int end = 0;
         private int whichProgress = 0;//需要下载的是哪个分片
 
-        public DownLoadThread(int start,int end,int whichProgress){
+        public DownLoadThread(int start, int end, int whichProgress) {
             this.start = start;
             this.start = end;
             this.whichProgress = whichProgress;
@@ -72,6 +94,46 @@ public class HttpDownloader1 {
         public void run() {
             CloseableHttpClient httpclient = HttpClients.createDefault();
 
+        }
+    }
+
+
+    /**
+     * 关于下载过程中的进程状态
+     */
+    enum ProgressStatus{
+        //0:未开始，1：下载完成，2：下载中，3：写入完成，4：写入中
+        NO_START(0,"未开始"),
+        DOWNLOAD_OK(1,"下载完成"),
+        DOWNLOAD_ING(2, "下载中"),
+        WRITE_OK(4, "写入完成"),
+        WRITTING(5,"写入中")
+
+        ;
+
+
+        private String value ;
+        private int key ;
+
+        private ProgressStatus( int key , String value ){
+            this.key = key ;
+            this.value = value ;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public void setKey(int key) {
+            this.key = key;
         }
     }
 }
