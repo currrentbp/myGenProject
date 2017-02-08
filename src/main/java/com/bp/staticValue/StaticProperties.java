194168f610afe2b79063250032d40786977382c4
wb-baopan <wb-baopan@le.com>
2017-02-08 14:55:00 +0800

    添加一个静态的常量

---
+++ src/main/java/com/bp/staticValue/StaticProperties.java
@@ -0,0 +1,16 @@
+package com.bp.staticValue;
+
+/**
+ * Created by issuser on 2017/2/8.
+ */
+public class StaticProperties {
+
+    public static int ZERO_INT = 0;
+    public static int ONE_INT = 1;
+
+    public static long ZERO_LONG = 0L;
+    public static long ONE_LONG = 1L;
+
+
+
+}
