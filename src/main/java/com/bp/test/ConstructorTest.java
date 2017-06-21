79aa99a123fb5e5ce31ef914e792fa69be4c7d9c
wb-baopan <wb-baopan@le.com>
2017-06-21 10:33:12 +0800

    添加一个关于short的测试类，添加一个关于构造函数的测试类

---
+++ src/main/java/com/bp/test/ConstructorTest.java
@@ -0,0 +1,26 @@
+package com.bp.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 关于构造函数的测试类
+ *
+ * @author current_bp
+ * @createTime 20170621
+ */
+public class ConstructorTest {
+    private static Logger logger = LoggerFactory.getLogger(ConstructorTest.class);
+
+    /*
+    在外部类中使用构造方法时，编译报错
+     */
+    private ConstructorTest() {
+        logger.info("===========");
+    }
+
+    public static void main(String[] args) {
+        ConstructorTest constructorTest = new ConstructorTest();
+
+    }
+}
