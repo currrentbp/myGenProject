f883d45d300c7936b2ceec3ed41ce24c5e358de4
baopan <baopan@gaosiedu.com>
2017-07-18 11:42:24 +0800

    测试拷贝bean对象

---
+++ src/main/java/com/bp/bean/copy/Other.java
@@ -0,0 +1,17 @@
+package com.bp.bean.copy;
+
+/**
+ * @author current_bp
+ * @createTime 20170718
+ */
+public class Other {
+    private Integer o1;
+
+    public Integer getO1() {
+        return o1;
+    }
+
+    public void setO1(Integer o1) {
+        this.o1 = o1;
+    }
+}
---
+++ src/main/java/com/bp/bean/copy/Second.java
@@ -0,0 +1,28 @@
+package com.bp.bean.copy;
+
+import java.util.Date;
+
+/**
+ * @author current_bp
+ * @createTime 20170718
+ */
+public class Second {
+    private String se1;
+    private Date date;
+
+    public String getSe1() {
+        return se1;
+    }
+
+    public void setSe1(String se1) {
+        this.se1 = se1;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
