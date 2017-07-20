1fe6d5129fa1b5937dedfe480b7579bcbd693c66
baopan <baopan@gaosiedu.com>
2017-07-20 18:27:21 +0800

    一些加密的代码，一些加密、解密的测试方法

---
+++ src/main/java/com/bp/util/all/encryption/Base64Util.java
@@ -0,0 +1,52 @@
+package com.bp.util.all.encryption;
+
+import com.bp.util.all.CheckUtil;
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author current_bp
+ * @createTime 20170523
+ */
+public class Base64Util {
+    /**
+     * 解密
+     *
+     * @param value
+     * @return
+     * @Method：解密
+     */
+    public static String decodeBase64(String value) {
+        if (CheckUtil.isEmpty(value)) {
+            return null;
+        }
+        String result = null;
+        try {
+            //此处默认数据来源是utf-8的
+            result = new String(Base64.decodeBase64(value.getBytes("utf-8")), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * @param value
+     * @return
+     * @Method：加密
+     */
+    public static String encodeBase64(String value) {
+        if (CheckUtil.isEmpty(value)) {
+            return null;
+        }
+        String result = null;
+        try {
+            //此处默认数据来源是utf-8的
+            result = new String(Base64.encodeBase64(value.getBytes("utf-8")), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/bp/util/all/encryption/RSACoderTest.java
@@ -0,0 +1,47 @@
+package com.bp.util.all.encryption;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2017/7/20.
+ */
+public class RSACoderTest {
+    @Test
+    public void sign() throws Exception {
+    }
+
+    @Test
+    public void verify() throws Exception {
+    }
+
+    @Test
+    public void decryptByPrivateKey() throws Exception {
+    }
+
+    @Test
+    public void decryptByPublicKey() throws Exception {
+    }
+
+    @Test
+    public void encryptByPublicKey() throws Exception {
+    }
+
+    @Test
+    public void encryptByPrivateKey() throws Exception {
+    }
+
+    @Test
+    public void getPrivateKey() throws Exception {
+    }
+
+    @Test
+    public void getPublicKey() throws Exception {
+    }
+
+    @Test
+    public void initKey() throws Exception {
+    }
+
+}
