8688e2c28324f2c0b9b3a51ecbd73b69f4edc8cd
baopan <baopan@gaosiedu.com>
2017-08-31 14:51:45 +0800

    add test,and add some entity

---
+++ src/main/java/com/bp/common/entity/Course.java
@@ -0,0 +1,42 @@
+package com.bp.common.entity;
+
+/**
+ * 学科
+ * @author current_bp
+ * @createTime 20170831
+ */
+public class Course {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Course course = (Course) o;
+
+        return id != null ? id.equals(course.id) : course.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
--- src/test/java/com/bp/util/all/MapUtilTest.java
+++ src/test/java/com/bp/util/all/MapUtilTest.java
@@ -1,8 +1,11 @@
 package com.bp.util.all;
 
 import com.alibaba.fastjson.JSON;
+import com.bp.common.entity.Student;
 import com.bp.daletou.DaletouEntity;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +18,8 @@ import static org.junit.Assert.*;
  * @createTime 20170830
  */
 public class MapUtilTest {
+    private final static Logger logger = LoggerFactory.getLogger(MapUtilTest.class);
+
     @Test
     public void getMapByList() throws Exception {
 
@@ -28,6 +33,22 @@ public class MapUtilTest {
 
         Map<Integer, DaletouEntity> map = MapUtil.getMapByList(daletouEntities, "id");
         System.out.println("===>map:" + JSON.toJSONString(map));
+    }
+
+    @Test
+    public void getMapByList2() throws Exception {
+        List<Student> students = new ArrayList<Student>();
+        Student student = new Student();
+        student.setId(1);
+        student.setName("1");
+        students.add(student);
+        Student student2 = new Student();
+        student2.setId(2);
+        student2.setName("2");
+        students.add(student2);
+        Map<Integer, Student> map = MapUtil.getMapByList(students, "id");
+        logger.info("===>map:" + JSON.toJSONString(map));
+
     }
 
 }
