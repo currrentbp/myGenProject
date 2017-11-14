a9486127f6f99f689702368492dad168e0100b28
baopan <baopan@gaosiedu.com>
2017-11-14 09:57:24 +0800

    add junit order test

--- src/test/java/com/currentbp/util/all/ListUtilTest.java
+++ src/test/java/com/currentbp/util/all/ListUtilTest.java
@@ -8,10 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by Administrator on 2017/8/30.
+ * @author current_bp
+ * @createTime 20170830
  */
 public class ListUtilTest {
     private final static Logger logger = LoggerFactory.getLogger(ListUtilTest.class);
@@ -47,6 +49,21 @@ public class ListUtilTest {
 
     @Test
     public void stringList2LongList() throws Exception {
+        String s1 = "1.0";
+        String s2 = "2.0";
+        System.out.println(s1.compareTo(s2));
+
+        String s3 = "2.09";
+        String s4 = "2.0";
+        System.out.println(s3.compareTo(s4));
+
+        String s5 = "2.0";
+        String s6 = "2.0.1";
+        System.out.println(s5.compareTo(s6));
+
+        String s7 = "2.0.01";
+        String s8 = "2.0.1";
+        System.out.println(s7.compareTo(s8));
     }
 
     @Test
