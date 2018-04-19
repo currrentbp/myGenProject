63f4a330eea5dedde98a9564f0304d8e4bbd6c7a
baopan <baopan@gaosiedu.com>
2018-04-19 18:20:51 +0800

    snow flake get ID

--- src/main/java/com/currentbp/ASM/ASMClassAdapter.java
+++ src/main/java/com/currentbp/ASM/ASMClassAdapter.java
@@ -3,6 +3,8 @@ package com.currentbp.ASM;
 import net.sf.cglib.asm.ClassAdapter;
 import net.sf.cglib.asm.ClassVisitor;
 
+import java.util.HashMap;
+
 /**
  * 字节码增强
  *
@@ -12,5 +14,7 @@ import net.sf.cglib.asm.ClassVisitor;
 public class ASMClassAdapter extends ClassAdapter {
     public ASMClassAdapter(ClassVisitor classVisitor) {
         super(classVisitor);
+    }
+    public void t1(){
     }
 }
