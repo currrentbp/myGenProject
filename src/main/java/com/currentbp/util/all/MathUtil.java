6b26a9bd22b7f322ab92a85812815e97e2b8a363
baopan <baopan@gaosiedu.com>
2018-04-27 09:24:09 +0800

    Cmn

--- src/main/java/com/currentbp/util/all/MathUtil.java
+++ src/main/java/com/currentbp/util/all/MathUtil.java
@@ -16,6 +16,42 @@ import java.util.List;
 public class MathUtil {
     private final static Logger logger = LoggerFactory.getLogger(MathUtil.class);
 
+    public static long c(int m, int n) {
+        Assert.isTrue(0<m && m <= m, "数学表达式不正确，0<m<=m");
+        long result = factorial(m, n) / factorial(m);
+
+        return result;
+    }
+
+    /**
+     * 阶乘:A!
+     *
+     * @param n 数值
+     * @return 结果
+     */
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    /**
+     * 组合:Amn
+     *
+     * @param m 上数
+     * @param n 下数
+     * @return 结果
+     */
+    public static long factorial(int m, int n) {
+        long result = 1;
+        for (int i = n - m + 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
     /**
      * 组合:Cmn
      *
