f4bc9bdc1e2de395c45929362a8fd47e8fb80acd
baopan <baopan@gaosiedu.com>
2018-06-01 11:13:17 +0800

    ssss

--- src/main/java/com/currentbp/test/AsynTest.java
+++ src/main/java/com/currentbp/test/AsynTest.java
@@ -23,13 +23,17 @@ public class AsynTest {
     }
 
     public static void main(String[] args) {
+        String sf = "====================================++++++++++++++++++++++++++++++++++++++===================================="
+                +"sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+                +"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+                +"ssssssssdfffffffffffffffffffffffffffggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                +"11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+                +"3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333";
         for(int i=1;i<=1000;i++) {
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    Map<String, Object> params = new HashMap<String, Object>();
-                    params.put("city", AsynTest.getNext());
-                    HttpResultData httpResultData = HTTPUtil.postRequest("http://localhost:8080/myWebProject/bp/c1", params);
+                    HttpResultData httpResultData = HTTPUtil.postRequest("http://localhost:8080/myWebProject/bp/c1", sf+AsynTest.getNext());
                     System.out.println(httpResultData);
                     if ("error".equals(httpResultData.getBody())) {
                         System.out.println("has error");
--- src/main/java/com/currentbp/util/all/HTTPUtil.java
+++ src/main/java/com/currentbp/util/all/HTTPUtil.java
@@ -9,12 +9,18 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 
 /**
@@ -62,6 +68,64 @@ public class HTTPUtil {
         return result;
     }
 
+    public static HttpResultData postRequest(String url, String source){
+        return postRequest(url,source,"","",5000,5000);
+    }
+    public static HttpResultData postRequest(String url, String source, String contentType, String charset, int socketTimeOut, int connectTimeout) {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        HttpResultData result = new HttpResultData(HttpResultData.ERROR,HttpResultData.IS_OTHER_ERROR,"");
+        String temp = "";
+        try {
+            URL realUrl = new URL(url);
+            // 打开和URL之间的连接
+            URLConnection conn = realUrl.openConnection();
+            // 设置通用的请求属性
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            // 获取URLConnection对象对应的输出流
+            out = new PrintWriter(conn.getOutputStream());
+            // 发送请求参数
+            out.print(source);
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                temp += line;
+            }
+            result.setBody(temp).setCode(200);
+        } catch (Exception e) {
+            System.out.println("发送 POST 请求出现异常！"+e);
+            e.printStackTrace();
+            result.setBody(e.getMessage());
+        }
+        //使用finally块来关闭输出流、输入流
+        finally{
+            try{
+                if(out!=null){
+                    out.close();
+                }
+                if(in!=null){
+                    in.close();
+                }
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+                result.setBody(ex.getMessage());
+            }
+        }
+        return result;
+    }
+
+
     /**
      * http post 请求
      *
@@ -82,7 +146,7 @@ public class HTTPUtil {
      * @param charset        字符集
      * @param socketTimeOut  超时时间
      * @param connectTimeout 连接超时时间
-     * @return
+     * @return 请求结果
      */
     public static HttpResultData postRequest(String url, Map<String, Object> params, String contentType, String charset, int socketTimeOut, int connectTimeout) {
         CloseableHttpClient httpclient = HttpClients.createDefault();
