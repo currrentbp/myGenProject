efcf2a03e6e61898ac086431b283fa1aeff15a5e
currrentbp <1403271248@qq.com>
2019-04-12 13:29:43 +0800

    SSS sssss

--- src/main/java/com/currentbp/Interesting/likou/T00031.java
+++ src/main/java/com/currentbp/Interesting/likou/T00031.java
@@ -1,10 +1,12 @@
 package com.currentbp.Interesting.likou;
 
+
+import com.currentbp.util.all.ListUtil;
 import org.junit.Test;
 
 /**
  * @author baopan
- * @createTime 20190403
+ * @createTime 2019/4/6 9:34
  */
 public class T00031 {
     /*
@@ -19,12 +21,50 @@ public class T00031 {
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
      */
+
     @Test
-    public void t1(){
-
+    public void t1() {
+        int[] nums4 = new int[]{1,3,2};
+        nextPermutation(nums4);
+        ListUtil.printList(nums4);
+        int[] nums1 = new int[]{1,2,3};
+        nextPermutation(nums1);
+        ListUtil.printList(nums1);
+        int[] nums2 = new int[]{3,2,1};
+        nextPermutation(nums2);
+        ListUtil.printList(nums2);
+        int[] nums3 = new int[]{1,1,5};
+        nextPermutation(nums3);
+        ListUtil.printList(nums3);
     }
-    public void nextPermutation(int[] nums) {
 
+    public void nextPermutation(int[] nums) {
+        if (null == nums || nums.length <= 1) {
+            return;
+        }
+        boolean flag = true;
+        int index = 0;
+        for (int i = 0; i + 1 != nums.length; i++) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                flag = false;
+            }
+        }
+
+        if (!flag) {
+            int temp = nums[index];
+            nums[index] = nums[index + 1];
+            nums[index + 1] = temp;
+        } else {
+            int left = 0, right = nums.length - 1;
+            while (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
 
     }
 }
