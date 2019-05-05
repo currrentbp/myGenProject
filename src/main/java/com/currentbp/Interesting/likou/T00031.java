8312543d1d7fe82ec1778190d970bf6fb9f1c997
currrentbp <1403271248@qq.com>
2019-05-05 21:14:36 +0800

    ss

--- src/main/java/com/currentbp/Interesting/likou/T00031.java
+++ src/main/java/com/currentbp/Interesting/likou/T00031.java
@@ -21,74 +21,94 @@ public class T00031 {
 必须原地修改，只允许使用额外常数空间。
 
 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+1,3,2 → 2,1,3
 1,2,3 → 1,3,2
 3,2,1 → 1,2,3
 2,3,1 → 3,1,2
 1,1,5 → 1,5,1
      */
-    @Test
-    public void t3(){
-        Map<Integer,String> ss = new HashMap<>();
-        ss.put(1,"ssss");
-        ss.put(2,"ffff");
-        Collection<String> values = ss.values();
-        ListUtil.printList(Lists.newArrayList(values));
 
-    }
     @Test
-    public void t2(){
-        List<Student> students = new ArrayList<>();
-        students.add(new Student(1,"1111"));
-        students.add(new Student(2,"2222"));
-        Student student = students.get(0);
-        students.remove(0);
-        System.out.println(students);
-        System.out.println(student);
+    public void t2() {
+        for (int i = -1; i >= 0; i--) {
+            System.out.println("=======");
+        }
     }
 
     @Test
     public void t1() {
-        int[] nums4 = new int[]{1,3,2};
-        nextPermutation(nums4);
-        ListUtil.printList(nums4);
-        int[] nums1 = new int[]{1,2,3};
-        nextPermutation(nums1);
-        ListUtil.printList(nums1);
-        int[] nums2 = new int[]{3,2,1};
-        nextPermutation(nums2);
-        ListUtil.printList(nums2);
-        int[] nums3 = new int[]{1,1,5};
-        nextPermutation(nums3);
-        ListUtil.printList(nums3);
+        int[] nums5= new int[]{4,2,0,2,3,2,0};
+        nextPermutation(nums5);
+        ListUtil.printList(nums5);
+//        int[] nums4 = new int[]{1, 3, 2};
+//        nextPermutation(nums4);
+//        ListUtil.printList(nums4);
+//        int[] nums1 = new int[]{1, 2, 3};
+//        nextPermutation(nums1);
+//        ListUtil.printList(nums1);
+//        int[] nums2 = new int[]{3, 2, 1};
+//        nextPermutation(nums2);
+//        ListUtil.printList(nums2);
+//        int[] nums3 = new int[]{1, 1, 5};
+//        nextPermutation(nums3);
+//        ListUtil.printList(nums3);
     }
 
     public void nextPermutation(int[] nums) {
         if (null == nums || nums.length <= 1) {
             return;
         }
-        boolean flag = true;
-        int index = 0;
-        for (int i = 0; i + 1 != nums.length; i++) {
-            if (nums[i] < nums[i + 1]) {
-                index = i;
-                flag = false;
+
+        int start = -1;
+        boolean flag = false;
+        for (int right = nums.length - 1; right >= 0; right--) {
+            if (flag) {
+                break;
+            }
+            for (int left = right - 1; left >= 0; left--) {
+                if (nums[left] < nums[right]) {
+                    flag = true;
+                    swap(nums, left, right);
+                    start = left;
+                    break;
+                }
             }
         }
-
         if (!flag) {
-            int temp = nums[index];
-            nums[index] = nums[index + 1];
-            nums[index + 1] = temp;
+            swapEach(nums);
         } else {
-            int left = 0, right = nums.length - 1;
-            while (left < right) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-                right--;
-            }
+            ascSort(nums, start+1);
         }
+    }
 
+    private void ascSort(int[] nums, int start) {
+        for (int i = start; i < nums.length; i++) {
+            int minIndex = i;
+            int tempMin = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < tempMin) {
+                    minIndex = j;
+                    tempMin = nums[j];
+                }
+            }
+            swap(nums, i, minIndex);
+        }
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+
+    private void swapEach(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
