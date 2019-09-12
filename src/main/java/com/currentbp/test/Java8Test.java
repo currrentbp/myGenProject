90dd377610b05da807b3e16df101fe77c4131ea3
baopan <baopan@gaosiedu.com>
2019-09-12 18:34:19 +0800

    ss

--- src/main/java/com/currentbp/test/Java8Test.java
+++ src/main/java/com/currentbp/test/Java8Test.java
@@ -1,9 +1,12 @@
 package com.currentbp.test;
 
+import com.currentbp.common.entity.Student;
 import com.currentbp.util.all.ListUtil;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -13,6 +16,31 @@ import static java.util.stream.Collectors.toList;
  * @createTime 20190823
  */
 public class Java8Test {
+
+    @Test
+    public void sort(){
+        List<Student> students = new ArrayList<>(2);
+        Student student1 = new Student(2,"ss");
+        Student student2 = new Student(3,"ff");
+        students.add(student1);
+        students.add(student2);
+
+        List<Student> collect = students.stream().sorted((x, y) -> {
+            return 3 == x.getId() ? -1 : 1;
+        }).collect(toList());
+        ListUtil.printList(collect);
+    }
+    @Test
+    public void t2(){
+        List<Student> students = new ArrayList<>(2);
+        Student student1 = new Student(2,"ss");
+        Student student2 = new Student(3,"ff");
+        students.add(student1);
+        students.add(student2);
+        int sum = students.stream().mapToInt(Student::getId).sum();
+        System.out.println(sum);
+    }
+
     @Test
     public void t1(){
         String[] words = new String[]{"Hello","World"};
--- src/main/java/com/currentbp/test/ObjectTest.java
+++ src/main/java/com/currentbp/test/ObjectTest.java
@@ -2,9 +2,13 @@ package com.currentbp.test;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.currentbp.common.entity.Student;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author current_bp
@@ -12,6 +16,35 @@ import org.slf4j.LoggerFactory;
  */
 public class ObjectTest {
     private final static Logger logger = LoggerFactory.getLogger(ObjectTest.class);
+
+
+    @Test
+    public void t2(){
+        Object student1 = new Student();
+        Object students = new ArrayList<Student>();
+        System.out.println(student1.getClass());
+        System.out.println(students.getClass());
+
+        if(student1 instanceof Student){
+            System.out.println("+++++++++++++++");
+        }else{
+            System.out.println("----------------");
+        }
+        if(students instanceof List){
+            System.out.println("+++++++++++++++");
+        }
+        if(students instanceof ArrayList){
+            System.out.println("+++++++++++++++===============");
+        }else {
+            System.out.println("---------------");
+        }
+        student1 = null;
+        if(student1 instanceof Student){
+            System.out.println("+++++++++++++++");
+        }else{
+            System.out.println("----------------");
+        }
+    }
 
     @Test
     public void t1(){
