bb39ebb9662c1d07d86e6e8b12f7a69d914e3767
baopan <baopan@gaosiedu.com>
2020-04-02 20:07:10 +0800

    sss

--- src/test/java/com/currentbp/util/all/StringUtilTest.java
+++ src/test/java/com/currentbp/util/all/StringUtilTest.java
@@ -1,11 +1,42 @@
 package com.currentbp.util.all;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by issuser on 2017/5/25.
  */
 public class StringUtilTest {
+    @Test
+    public void t123(){
+        List<Integer> x = new ArrayList<>();
+        x.add(1);
+        x.add(2);
+        x.add(3);
+        x.add(4);
+        x.add(5);
+        List<Integer> list = x.subList(0, 3);
+        StringUtil.printObject(list);
+
+    }
+
+    @Test
+    public void t1(){
+        List<Integer> s1 = new ArrayList<>(4);
+        s1.add(1);
+        s1.add(5);
+        s1.add(3);
+
+        Collections.sort(s1);
+        s1.set(3,2);
+        Collections.reverse(s1);
+        StringUtil.printObject(s1);
+    }
+
     @Test
     public void getHumpFormat(){
         System.out.println(StringUtil.getHumpFormat("bp"));
