2a953f33b0c5cd83729144122642b6f1046b7970
baopan <baopan@xiaomi.com>
2021-03-29 17:35:12 +0800

    test :add description

--- src/main/java/com/currentbp/test/collectionTest/MapTest.java
+++ src/main/java/com/currentbp/test/collectionTest/MapTest.java
@@ -20,6 +20,7 @@ public class MapTest {
         users.put(1L, 1L);
         users.put(2L, 2L);
         users.put(3L, 3L);
+        //ssssss
         Map<Long, Long> noRobotUsers = users.entrySet().stream()
                 .filter(kv -> kv.getKey() != 2L)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
