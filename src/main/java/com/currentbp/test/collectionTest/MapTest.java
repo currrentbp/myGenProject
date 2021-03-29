144e1ecef872a012769a17ce2aad019526291ecb
baopan <baopan@xiaomi.com>
2021-03-29 17:34:18 +0800

    fff

---
+++ src/main/java/com/currentbp/test/collectionTest/MapTest.java
@@ -0,0 +1,28 @@
+package com.currentbp.test.collectionTest;
+
+import com.currentbp.util.all.StringUtil;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author baopan
+ * @createTime 20210324
+ */
+public class MapTest {
+
+
+    @Test
+    public void t1() {
+        Map<Long, Long> users = new HashMap<>();
+        users.put(1L, 1L);
+        users.put(2L, 2L);
+        users.put(3L, 3L);
+        Map<Long, Long> noRobotUsers = users.entrySet().stream()
+                .filter(kv -> kv.getKey() != 2L)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        StringUtil.printObject(noRobotUsers);
+    }
+}
