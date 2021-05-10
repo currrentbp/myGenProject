9884a2f2cbd14e501094a8716a876bcf819eddfd
baopan <baopan@xiaomi.com>
2021-05-10 19:47:58 +0800

    sss

--- src/main/java/com/currentbp/util/all/ElasticSearchSevenUtil.java
+++ src/main/java/com/currentbp/util/all/ElasticSearchSevenUtil.java
@@ -1,31 +1,28 @@
 package com.currentbp.util.all;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.currentbp.common.model.Student;
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.index.VersionType;
-import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
 
 
 /**
@@ -47,7 +44,7 @@ public class ElasticSearchSevenUtil {
             Object id = ClassUtil.getValue(obj, "id");
             insert(obj, null, id.toString());
         } catch (Exception e) {
-            System.out.println("ElasticSearchSevenUtil, getId is error, obj:" + JSON.toJSONString(obj)+" errorMsg:"+e.getMessage());
+            System.out.println("ElasticSearchSevenUtil, getId is error, obj:" + JSON.toJSONString(obj) + " errorMsg:" + e.getMessage());
         }
     }
 
@@ -80,13 +77,56 @@ public class ElasticSearchSevenUtil {
         }
     }
 
-    public void queryById(String index,String id){
+    public <T> T queryById(String index, String id, Class<T> t) {
         RestHighLevelClient client = getClient();
+        GetRequest getRequest = new GetRequest(index, id);
+        try {
+            GetResponse response = client.get(getRequest, RequestOptions.DEFAULT);
+            String sourceAsString = response.getSourceAsString();
+            System.out.println("queryById,source:" + sourceAsString);
+            T t1 = JSON.parseObject(sourceAsString, t);
+            return t1;
+        } catch (Exception e) {
+            System.out.println("queryById is error!index:" + index + ",id:" + id + ",msg:" + e.getMessage());
+        }
+        return null;
     }
 
-    public void query(String index, String type) {
+    public <T> List<T> queryByCondition(String index, Class<T> t) {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.size(1);
+        MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("logLevel", "INFO");
+        BoolQueryBuilder boolq = QueryBuilders.boolQuery();
+        boolq.must(matchQueryBuilder);
+        searchSourceBuilder.query(boolq);
 
+        SearchRequest searchRequest = new SearchRequest(index);
+        searchRequest.source(searchSourceBuilder);
+
+        return queryByCondition(index, t, searchRequest);
     }
+
+    public <T> List<T> queryByCondition(String index, Class<T> t, SearchRequest searchRequest) {
+        RestHighLevelClient client = getClient();
+        try {
+            SearchResponse search = client.search(searchRequest, RequestOptions.DEFAULT);
+            SearchHits hits = search.getHits();
+            Iterator<SearchHit> iterator = hits.iterator();
+            List<T> result = new ArrayList<>();
+            while (iterator.hasNext()) {
+                SearchHit next = iterator.next();
+                String sourceAsString = next.getSourceAsString();
+                System.out.println("queryByCondition,source:" + sourceAsString);
+                T t1 = JSON.parseObject(sourceAsString, t);
+                result.add(t1);
+            }
+            return result;
+        } catch (Exception e) {
+            System.out.println("queryByCondition is error!index:" + index + ",msg:" + e.getMessage());
+        }
+        return null;
+    }
+
 
     public RestHighLevelClient getClient() {
         RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost(esHost, esPort, "http")));
