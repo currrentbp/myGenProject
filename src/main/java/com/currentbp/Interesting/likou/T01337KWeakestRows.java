6870aef0ef5023c0f96fe4ae0657ec5adbf200b3
baopan <baopan@xiaomi.com>
2021-09-14 15:20:27 +0800

    sss

--- src/main/java/com/currentbp/Interesting/likou/T01337KWeakestRows.java
+++ src/main/java/com/currentbp/Interesting/likou/T01337KWeakestRows.java
@@ -3,6 +3,10 @@ package com.currentbp.Interesting.likou;
 import com.currentbp.util.all.StringUtil;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author baopan
  * @createTime 20210801
@@ -14,18 +18,33 @@ public class T01337KWeakestRows {
 如果第i行的军人数量少于第j行，或者两行军人数量相同但 i 小于 j，那么我们认为第 i 行的战斗力比第 j 行弱。
 军人 总是 排在一行中的靠前位置，也就是说 1 总是出现在 0 之前。
      */
+    @Test
+    public void t3(){
+        System.out.println(new Date());
+    }
 
     @Test
-    public void t1(){
+    public void t1() {
         int[][] mat = {{1, 1, 0, 0, 0},
                 {1, 1, 1, 1, 0},
                 {1, 0, 0, 0, 0},
                 {1, 1, 0, 0, 0},
                 {1, 1, 1, 1, 1}};
-        StringUtil.printObject(kWeakestRows(mat,3));
+        StringUtil.printObject(kWeakestRows(mat, 3));
     }
 
     public int[] kWeakestRows(int[][] mat, int k) {
+        List<Integer> values = new ArrayList(mat.length);
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                values.set(i, values.get(i) + mat[i][j]);
+            }
+        }
+        List<Integer> result = new ArrayList<>(k);
+        for (int i = 0; i < values.size(); i++) {
+
+
+        }
 
         return new int[k];
     }
