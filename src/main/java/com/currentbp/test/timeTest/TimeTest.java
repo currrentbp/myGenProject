534801d1cc756c1e52f00479c0efc2bb663c46a1
baopan <baopan@xiaomi.com>
2022-08-22 21:04:39 +0800

    add T0072

--- src/main/java/com/currentbp/test/timeTest/TimeTest.java
+++ src/main/java/com/currentbp/test/timeTest/TimeTest.java
@@ -2,7 +2,19 @@ package com.currentbp.test.timeTest;
 
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class TimeTest {
+
+    @Test
+    public void testWeeks(){
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int week = cal.get(Calendar.DAY_OF_WEEK);
+        System.out.println(week);
+    }
     @Test
     public void t1(){
 
