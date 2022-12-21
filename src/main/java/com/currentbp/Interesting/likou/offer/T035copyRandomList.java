94e3a827d66e7489ac49614a9bed2be9c004c655
baopan <baopan@xiaomi.com>
2022-12-21 19:55:10 +0800

    sss

--- src/main/java/com/currentbp/Interesting/likou/offer/T035copyRandomList.java
+++ src/main/java/com/currentbp/Interesting/likou/offer/T035copyRandomList.java
@@ -8,21 +8,48 @@ import com.currentbp.common.entity.Node;
  */
 public class T035copyRandomList {
     public Node copyRandomList(Node head) {
-        if(head == null){
+        if (head == null) {
             return null;
         }
 
         Node result = new Node(0);
         Node before = result;
         Node temp = head;
-        while(temp != null){
+        while (temp != null) {
             Node node1 = new Node(temp.val);
-            before.next=node1;
+            before.next = node1;
             before = node1;
+
+            temp = temp.next;
         }
 
         Node copyNode = result.next;
 
+        Node indexHead = head;
+        Node indexCopyHead = copyNode;
 
+        while (indexHead != null) {
+            if (indexHead.random == null) {
+                indexHead = indexHead.next;
+                indexCopyHead = indexCopyHead.next;
+                continue;
+            }
+
+            Node random = indexHead.random;
+
+            Node cursorHead = head;
+            Node cursorCopyHead = copyNode;
+            while (cursorHead != null) {
+                if (cursorHead == random) {
+                    break;
+                } else {
+                    cursorHead = cursorHead.next;
+                    cursorCopyHead = cursorCopyHead.next;
+                }
+            }
+            indexCopyHead.random = cursorCopyHead;
+
+        }
+        return copyNode;
     }
 }
