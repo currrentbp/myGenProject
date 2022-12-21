f8f9d37e22b71bb3185718ba75031740b0854d22
baopan <baopan@xiaomi.com>
2022-12-21 20:23:04 +0800

    sss3

--- src/main/java/com/currentbp/Interesting/likou/offer/T058reverseLeftWords.java
+++ src/main/java/com/currentbp/Interesting/likou/offer/T058reverseLeftWords.java
@@ -1,7 +1,22 @@
 package com.currentbp.Interesting.likou.offer;
 
-public class T058reverseLeftWords {
-    public String reverseLeftWords(String s, int n) {
+import org.junit.Test;
 
+/**
+ * @author baopan
+ * @createTime 20221221
+ */
+public class T058reverseLeftWords {
+
+    @Test
+    public void t1(){
+        System.out.println(reverseLeftWords("abcdefg",2));
+        System.out.println(reverseLeftWords("lrloseumgh",6));
+    }
+
+    public String reverseLeftWords(String s, int n) {
+        String head = s.substring(0,n);
+        String tail = s.substring(n);
+        return tail+head;
     }
 }
