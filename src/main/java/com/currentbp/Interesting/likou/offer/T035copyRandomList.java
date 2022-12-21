1e99bcb4b1c2033696ecb1c13fbe2df86cb79de4
baopan <baopan@xiaomi.com>
2022-12-21 20:13:00 +0800

    sss2

--- src/main/java/com/currentbp/Interesting/likou/offer/T035copyRandomList.java
+++ src/main/java/com/currentbp/Interesting/likou/offer/T035copyRandomList.java
@@ -2,6 +2,11 @@ package com.currentbp.Interesting.likou.offer;
 
 import com.currentbp.common.entity.Node;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author baopan
  * @createTime 20221220
@@ -50,6 +55,45 @@ public class T035copyRandomList {
             indexCopyHead.random = cursorCopyHead;
 
         }
+        return copyNode;
+    }
+
+
+    public Node copyRandomList2(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Node result = new Node(0);
+        Node before = result;
+        Node temp = head;
+
+        Map<Node,Node> kvs = new HashMap<>();
+        while (temp != null) {
+            Node node1 = new Node(temp.val);
+            before.next = node1;
+            before = node1;
+
+            kvs.put(temp,node1);
+
+            temp = temp.next;
+        }
+
+        Node copyNode = result.next;
+
+        for (Map.Entry<Node, Node> kv : kvs.entrySet()) {
+            Node key = kv.getKey();
+            Node value = kv.getValue();
+
+            Node random = key.random;
+            if(random == null){
+                continue;
+            }
+
+            Node node = kvs.get(random);
+            value.random = node;
+        }
+
         return copyNode;
     }
 }
