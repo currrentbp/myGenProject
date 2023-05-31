7a8c531dbbeab9c2c9042559c5a48173a324d4b9
baopan <1403271248@qq.com>
2023-05-31 22:49:42 +0800

    ss

--- src/main/java/com/currentbp/test/spring/aop/jdkProxy/OnlyInterfaceProxy.java
+++ src/main/java/com/currentbp/test/spring/aop/jdkProxy/OnlyInterfaceProxy.java
@@ -19,16 +19,16 @@ public class OnlyInterfaceProxy implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-//        if (Object.class.equals(method.getDeclaringClass())) {
-//            return method.invoke(this, args);
-//        }
-        StringUtil.printObject(args);
-        System.out.println("调用前，参数：{}" + args);
-        //这里可以得到参数数组和方法等，可以通过反射，注解等，进行结果集的处理
-        //mybatis就是在这里获取参数和相关注解，然后根据返回值类型，进行结果集的转换
         int arg = (Integer)args[0];
         Student result = new Student();
         result.setId(arg);
+
+        if (Object.class.equals(method.getDeclaringClass())) {
+            return method.invoke(this, args);
+        }
+        StringUtil.printObject(args);
+        System.out.println("调用前，参数：{}" + args);
+
         return result;
     }
 }
